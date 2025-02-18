606cb2ad74e5183a82c29a40cfea4621cb4f2280
emosweaty <ymkeschoofs04@gmail.com>
2025-02-18 17:10:56 +0100

    Detailpage now has information

--- app/[sighting].tsx
+++ app/[sighting].tsx
@@ -1,11 +1,64 @@
 import { Stack, useLocalSearchParams } from "expo-router";
-import { View } from "react-native";
+import { useEffect, useState } from "react";
+import { View, StyleSheet, Text } from "react-native";
+
+import getSightings from "../services/getSightingsService";
+
+interface Sighting {
+    id: string
+    witnessName: string;
+    location: {
+        latitude: string;
+        longitude: string;
+    }
+    description: string;
+    status: string;
+
+}
+
+//TODO kan dit korter?
 
 export default function details(){
     const { sighting } = useLocalSearchParams<{sighting: string}>();
+    const [ detail, setDetail ] = useState<Sighting>(); 
+
+    useEffect(()=>{
+            fetchSightings()
+        }, []);
+
+    async function fetchSightings() {
+        try {
+            const data = await getSightings(); 
+            const found = data.find((item: Sighting) => item.witnessName === sighting);
+            setDetail(found); 
+        } catch (err) {
+            console.error("Failed to fetch sightings", err);
+        }
+    }
+
     return(
         <View>
             <Stack.Screen options={{title: sighting}}></Stack.Screen>
+            <Text>Location: {detail?.location.latitude}, {detail?.location.longitude}</Text>
+            <Text>Description: {detail?.description}</Text>
+            <Text>Status: {detail?.status}</Text>
         </View>
     )
 }
+
+ const styles = StyleSheet.create({
+        container: {
+            flex: 1,
+            justifyContent: "center",
+            alignItems: "center",
+            paddingTop: 50,
+        },
+        item: {
+            padding: 10,
+            borderBottomWidth: 1,
+            borderBottomColor: "#ddd",
+        },
+        product: {
+            fontSize: 18,
+        },
+});
